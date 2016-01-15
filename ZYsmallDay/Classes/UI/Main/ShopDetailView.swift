63b83aeffe2c09988a906d90e42f266e57d7bdd2
Misandy <misandy1021@gmail.com>
2016-01-15 23:53:43 +0800

    update

---
+++ ZYsmallDay/Classes/UI/Main/ShopDetailView.swift
@@ -0,0 +1,21 @@
+//
+//  ShopDetailView.swift
+//  ZYsmallDay
+//
+//  Created by 章宇 on 16/1/12.
+//  Copyright © 2016年 章宇. All rights reserved.
+//
+
+import UIKit
+
+class ShopDetailView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
